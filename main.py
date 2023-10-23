d2e5765a11ad92b8f228c4173bc5ef758e24ad9f
Wam25 <62901118+Wam25@users.noreply.github.com>
2023-10-23 12:30:15 -0400

    do more

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from random import *
 #         if mine:
 #             self.image
 
-grid_x = 8
-grid_y = 8
+grid_x = 16
+grid_y = 16
 n_mines = randint(10, (grid_x - 1) * (grid_y - 1))
 
 shuffled_mines = [0] * (grid_x * grid_y - n_mines) + [1] * (n_mines)
@@ -26,7 +26,7 @@ for i in range(grid_y):
     field.append(shuffled_mines[i * grid_x : i * grid_x + grid_x])
 
 for y, column in enumerate(field):
-    for x, line in enumerate(column):
+    for x, tile in enumerate(column):
         mines_around = 0
         if x > 0:
             if y > 0:
@@ -46,10 +46,11 @@ for y, column in enumerate(field):
             mines_around += field[y + 1][x]
         if mines_around == 8:
             field[x][y] = 0
+    print(column)
 
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([512, 512])
 
 inners = [pygame.transform.scale(pygame.image.load("./"+str(i)+"_Inner.png").convert(), (24, 24)) for i in range(1,9)]
 mine_tile = pygame.transform.scale(pygame.image.load("Mine_Bomb.png").convert(), (24, 24))
@@ -58,6 +59,19 @@ back_tile = pygame.transform.scale(pygame.image.load("Back_Tile.png").convert(),
 anger = pygame.transform.scale(pygame.image.load("Angry_Face.png").convert(), (48, 48))
 happy = pygame.transform.scale(pygame.image.load("Happy_Face.png").convert(), (48, 48))
 
+def on_click():
+    if pygame.mouse.get_pressed()[0]:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+    
+
+screen.fill((255, 255, 255))
+
+for y, column in enumerate(field):
+    for x, tile in enumerate(column):
+        screen.blit(back_tile, (32*x, 32*y))
+        if tile == 1:
+            screen.blit(mine_tile, (32*x+4, 32*y+4))
+    print(column)
 
 running = True
 
@@ -69,13 +83,13 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    
 
-    screen.blit(anger, (48,48))
-    screen.blit(happy, (96,48))
-    for i, image in enumerate(inners):
-        screen.blit(back_tile, (32*i, 0))
-        screen.blit(image, (32*i+4,4))
+    # for y, column in enumerate(field):
+    #     for x, tile in enumerate(column):
+    #         screen.blit(back_tile, (4*x*i, 4*y*i))
+    #         if tile == 1:
+    #             screen.blit(inners[0], (4*x*i+4, 4*x*i+4))
 
     pygame.display.flip()
 
