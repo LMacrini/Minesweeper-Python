26ca12e172a3065a902310f05f888a1cde3fc1a8
Wam25 <62901118+LMacrini@users.noreply.github.com>
2023-10-30 16:58:38 -0400

    make a usable field

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Importing Libraries
 import pygame
 from pygame.locals import (
     KEYDOWN,
@@ -6,76 +7,98 @@ from pygame.locals import (
 )
 from random import *
 
-# class Tile(pygame.sprite.Sprite):
-#     def __init__(self, mine):
-#         self.mine = mine
-#         self.image = pygame.Surface((8,8))
-#         self.image.fill(WHITE)
-#         if mine:
-#             self.image
-
-grid_x = 16
-grid_y = 16
-n_mines = randint(10, (grid_x - 3) * (grid_y - 3))
-
-shuffled_mines = [0] * (grid_x * grid_y - n_mines) + [1] * (n_mines)
-shuffle(shuffled_mines)
-
-field = []
-for i in range(grid_y):
-    field.append(shuffled_mines[i * grid_x : i * grid_x + grid_x])
-
-
-field =[[1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],[1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0],[0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0],[0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1],[0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1],[1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0],[0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],[0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1],[0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0],[1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1],[1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1],[1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0],[1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],[1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1],[0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1]]
-
-for y, column in enumerate(field):
-    for x, tile in enumerate(column):
-        mines_around = 0
-        if x > 0:
-            if y > 0:
-                mines_around += field[y - 1][x - 1]
-            if y < grid_y - 1:
-                mines_around += field[y + 1][x - 1]
-            mines_around += field[y][x - 1]
-        if x < grid_x - 1:
-            if y > 0:
-                mines_around += field[y - 1][x + 1]
-            if y < grid_y - 1:
-                mines_around += field[y + 1][x + 1]
-            mines_around += field[y][x - 1]
-        if y > 0:
-            mines_around += field[y - 1][x]
-        if y < grid_x - 1:
-            mines_around += field[y + 1][x]
-        if mines_around == 9:
-            field[x][y] = 0
-print("\n")
-for i in field:
-    print(i)
-
 pygame.init()
 
-screen = pygame.display.set_mode([512, 512])
+grid_x = 40
+grid_y = 20
+n_mines_ez = int(grid_x*grid_y / 10)
+n_mines = int(grid_x*grid_y / 6)
+n_mines_hard = int(grid_x*grid_y / 5)
+
+screen = pygame.display.set_mode([grid_x*32, grid_y*32])
 
 numbers = [pygame.image.load("ressources/"+str(i)+"_tile.png").convert() for i in range(10)]
-mine_tile = pygame.image.load("ressources/Mine_Bomb.png").convert()
+mine_tile = pygame.image.load("ressources/mine_tile.png").convert()
 flag_tile = pygame.image.load("ressources/flag_tile.png").convert()
 tile_image = pygame.image.load("ressources/tile.png").convert()
 anger = pygame.transform.scale(pygame.image.load("ressources/Angry_Face.png").convert(), (48, 48))
 happy = pygame.transform.scale(pygame.image.load("ressources/Happy_Face.png").convert(), (48, 48))
 
-def on_click():
-    if pygame.mouse.get_pressed()[0]:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, mine):
+        self.mine = mine
+        self.mines_around = 0
+        self.clicked = 0
+
+shuffled_mines = []
+for _ in range(grid_x * grid_y - n_mines):
+    shuffled_mines.append(Tile(0))
+for _ in range(n_mines):
+    shuffled_mines.append(Tile(1))
+shuffle(shuffled_mines)
+
+
+field = []
+for i in range(grid_y):
+    field.append(shuffled_mines[i * grid_x : i * grid_x + grid_x])
+
+for y, column in enumerate(field):
+    for x, tile in enumerate(column):
+        mines_around = 0
+        
+        if x > 0:
+            if y > 0:
+                mines_around += field[y - 1][x - 1].mine
+            if y < grid_y - 1:
+                mines_around += field[y + 1][x - 1].mine
+            mines_around += field[y][x - 1].mine
+            
+        if x < grid_x - 1:
+            if y > 0:
+                mines_around += field[y - 1][x + 1].mine
+            if y < grid_y - 1:
+                mines_around += field[y + 1][x + 1].mine
+            mines_around += field[y][x - 1].mine
+        
+        if y > 0:
+            mines_around += field[y - 1][x].mine
+        
+        if y < grid_y - 1:
+            mines_around += field[y + 1][x].mine
+        
+        if mines_around == 8:
+            field[y][x].mine = 0
+        
+        if field[y][x].mine == 0:
+            field[y][x].mines_around = mines_around
+            print(mines_around, field[y][x].mines_around)
+        
+        if field[y][x].mine:
+            field[y][x].image = mine_tile
+        else:
+            field[y][x].image = numbers[mines_around]
+
+# def on_click():
+#     if pygame.mouse.get_pressed()[0]:
+#         mouse_x, mouse_y = pygame.mouse.get_pos()
+#         mouse_x = int(mouse_x / 32)
+#         mouse_y = int(mouse_y / 32)
+#         field[mouse_y][mouse_x].clicked = 1
     
 
 screen.fill((255, 255, 255))
 
 for y, column in enumerate(field):
     for x, tile in enumerate(column):
-        screen.blit(tile_image, (32*x, 32*y))
-        if tile == 1:
-            screen.blit(mine_tile, (32*x+4, 32*y+4))
+        if tile.clicked == 0:
+            screen.blit(tile_image, (32*x, 32*y))
+        else:
+            screen.blit(tile.texture (32*x, 32*y))
+            # print(tile.mines_around)
+        # if tile == 1:
+        #     screen.blit(mine_tile, (32*x, 32*y))
+        # else:
+        #     screen.blit(tile_image, (32*x, 32*y))
 
 running = True
 
@@ -87,13 +110,20 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    
+        if pygame.mouse.get_pressed()[0] == 1 != previous_mouse_state:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            mouse_x = int(mouse_x / 32)
+            mouse_y = int(mouse_y / 32)
+            field[mouse_y][mouse_x].clicked = 1
+        
+        previous_mouse_state = pygame.mouse.get_pressed()[0]
 
-    # for y, column in enumerate(field):
-    #     for x, tile in enumerate(column):
-    #         screen.blit(back_tile, (4*x*i, 4*y*i))
-    #         if tile == 1:
-    #             screen.blit(inners[0], (4*x*i+4, 4*x*i+4))
+        for y, column in enumerate(field):
+            for x, tile in enumerate(column):
+                if tile.clicked == 0:
+                    screen.blit(tile_image, (32*x, 32*y))
+                else:
+                    screen.blit(tile.image, (32*x, 32*y))
 
     pygame.display.flip()
 
